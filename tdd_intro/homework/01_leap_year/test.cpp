826388a123c9a056045dd194724019a00fba82a1
roman808080 <roman.herasymenko@gmail.com>
2019-07-28 18:45:29 +0300

    Green test.

--- tdd_intro/homework/01_leap_year/test.cpp
+++ tdd_intro/homework/01_leap_year/test.cpp
@@ -17,10 +17,16 @@ namespace
 {
     bool is_leap_year(int year)
     {
+        if (year % 100 == 0)
+        {
+            return false;
+        }
+
         if (year % 4 == 0)
         {
             return true;
         }
+
         return false;
     }
 }
