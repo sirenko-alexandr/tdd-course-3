a27556249f1b7859ed6995ca04c4d163ae1c9f46
roman808080 <roman.herasymenko@gmail.com>
2019-09-27 23:34:11 +0300

    Added test with data from the task.

--- tdd_intro/demo/02_word_count/test.cpp
+++ tdd_intro/demo/02_word_count/test.cpp
@@ -17,6 +17,8 @@ such: 1
 #include <string>
 #include <map>
 
+static std::array<char, 3> s_symbosToRemove {' ', ',', '.'};
+
 std::string convertToLowerCase(const std::string& word)
 {
     std::string lowerCaseWord = "";
@@ -28,18 +30,43 @@ std::string convertToLowerCase(const std::string& word)
     return lowerCaseWord;
 }
 
+bool needToRemoveSymbol(const char symbol)
+{
+    for (const auto symbolToRemove : s_symbosToRemove)
+    {
+        if(symbol == symbolToRemove)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void stripAtPostion(std::string& string, unsigned long position)
+{
+    while (string.size() && needToRemoveSymbol(string[position]))
+    {
+        string.erase(position, 1);
+    }
+}
+
+void stripString(std::string& string)
+{
+    stripAtPostion(string, 0);
+    stripAtPostion(string, string.size() - 1);
+}
+
 std::string popWord(std::string& sentence)
 {
-    while (sentence.size() && sentence[0] == ' ')
-    {
-        sentence.erase(0, 1);
-    }
+    stripString(sentence);
 
     auto position = sentence.find_first_of(" ");
 
     auto word = sentence.substr(0, position);
     sentence.erase(0, position);
 
+    stripString(word);
     return convertToLowerCase(word);
 }
 
@@ -98,4 +125,19 @@ TEST(WordCount, PunctuationSymbols)
     auto words = countWords("comma, dot.");
     EXPECT_EQ(1, words["comma"]);
     EXPECT_EQ(1, words["dot"]);
+}
+
+TEST(WordCount, TaskData)
+{
+    auto words = countWords("olly olly in come free please please let it be in such manner olly");
+    EXPECT_EQ(3, words["olly"]);
+    EXPECT_EQ(2, words["in"]);
+    EXPECT_EQ(1, words["come"]);
+    EXPECT_EQ(1, words["free"]);
+    EXPECT_EQ(2, words["please"]);
+    EXPECT_EQ(1, words["let"]);
+    EXPECT_EQ(1, words["it"]);
+    EXPECT_EQ(1, words["be"]);
+    EXPECT_EQ(1, words["manner"]);
+    EXPECT_EQ(1, words["such"]);
 }
